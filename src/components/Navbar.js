b514c5c54a7b4cd471ebcda5a9754ba05f25c33a
a.bubelev <bubeleu@gmail.com>
2022-08-27 16:57:48 +0300

    click on hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
-import React from "react";
-import {FaBars, FaTimes} from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 import { icons } from "react-icons";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+
     return (
         <div className="header">
             <div className="container">
                 <h1>De<span className="primary">Fi</span></h1>
-                <ul className="nav-menu">
+                <ul className={click?'nav-menu active' : 'nav-menu'}>
                     <li>
                         <a href="/">Home</a>
                     </li>
@@ -25,8 +31,8 @@ const Navbar = () => {
                 <div className="btn-group">
                     <button className="btn">Connect Wallet</button>
                 </div>
-                <div className="hamburger">
-                    <FaBars size={20} style={{color:'#333'}}/>
+                <div className="hamburger"  onClick={handleClick}>
+                    <FaBars size={20} style={{ color: '#333' }} />
                 </div>
             </div>
 
