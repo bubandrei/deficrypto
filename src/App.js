fcbb8d056034162eef878d5e927bd419c2ca872a
a.bubelev <bubeleu@gmail.com>
2022-08-27 20:15:04 +0300

    create currency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Featured from "./components/Featured";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Featured />
     </>
   );
 }
---
+++ src/components/Featured.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './Featured.css';
+import BTC from '../assets/btc-img.png';
+import {FiArrowUpRight, FiArrowDownRight} from 'react-icons/fi';
+
+
+const Featured = () => {
+    return (
+        <div className="fetured">
+            <div className="container">
+                {/* left side */}
+                <div className="left">
+                    <h2>Explore top Crypto's Like Bitcoin, Ethereum, and Dogecoin</h2>
+                    <p>See all available assets: Cryptocurrencies and NFT's</p>
+                    <button className='btn'>See More Coins</button>
+                </div>
+
+
+                {/* right side */}
+                <div className="right">
+                    <div className="top">
+                        <img src={BTC} alt='/'/>
+                    </div>
+                    <div>
+                        <h5>Bitcoin</h5>
+                        <p>$49,000</p>
+                    </div>
+                    <span><FiArrowUpRight/>2.5%</span>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+export default Featured;
