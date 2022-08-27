f4bbac27fcfe632a28a155ed256f5906b789b905
a.bubelev <bubeleu@gmail.com>
2022-08-27 20:25:00 +0300

    axios for api.coingecko

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Featured.css';
 import BTC from '../assets/btc-img.png';
-import {FiArrowUpRight, FiArrowDownRight} from 'react-icons/fi';
+import { FiArrowUpRight, FiArrowDownRight } from 'react-icons/fi';
+import axios from "axios";
 
 
 const Featured = () => {
+
+    const [data, setData] = useState(null);
+
+
+
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false';
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [];)
+
+
+
+
     return (
         <div className="fetured">
             <div className="container">
@@ -19,13 +38,13 @@ const Featured = () => {
                 {/* right side */}
                 <div className="right">
                     <div className="top">
-                        <img src={BTC} alt='/'/>
+                        <img src={BTC} alt='/' />
                     </div>
                     <div>
                         <h5>Bitcoin</h5>
                         <p>$49,000</p>
                     </div>
-                    <span><FiArrowUpRight/>2.5%</span>
+                    <span><FiArrowUpRight />2.5%</span>
                 </div>
 
 
