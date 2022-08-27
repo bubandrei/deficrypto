32d63be4fb3c37000867fcaf089aa43dcf4fedff
a.bubelev <bubeleu@gmail.com>
2022-08-27 21:36:37 +0300

    some problems with navmenu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div className="header">
             <div className="container">
                 <h1>De<span className="primary">Fi</span></h1>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <ul className={click?'nav-menu active' : 'nav-menu'}>
                     <li>
                         <a href="/">Home</a>
                     </li>
@@ -31,9 +31,9 @@ const Navbar = () => {
                 <div className="btn-group">
                     <button className="btn">Connect Wallet</button>
                 </div>
-                <div className="hamburger" onClick={handleClick}>
-                    {click ? (<FaTimes size={20} style={{ color: '#333' }} />) : (<FaBars size={20} style={{ color: '#333' }} />)}
-
+                <div className="hamburger"  onClick={handleClick}>
+                    {click?(<FaTimes size={20} style={{ color: '#333' }}/>): (<FaBars size={20} style={{ color: '#333' }}/>)}
+                   
                 </div>
             </div>
 
