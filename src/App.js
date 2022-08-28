e23a1b2d2df3774884545de2a5dd5fa410b70e8d
a.bubelev <bubeleu@gmail.com>
2022-08-28 10:52:07 +0300

    html signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Featured from "./components/Featured";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Featured />
+      <Signup />
     </>
   );
 }
